edcfe6def5e12326ea3202398726e121bcc1d0fd
tiagoalfa <tiagoferreirafonsecaa@gmail.com>
2021-09-17 10:58:42 -0300

    Implementation API Users and Meals

---
+++ DevFitness.API/Controllers/UsersController.cs
@@ -0,0 +1,54 @@
+﻿using DevFitness.API.Core.Entities;
+using DevFitness.API.Models.InputModels;
+using DevFitness.API.Models.ViewModels;
+using DevFitness.API.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFitness.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DevFitnessDbContext _dbContext; 
+        public UsersController(DevFitnessDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var user = _dbContext.Users.SingleOrDefault(u=> u.Id == id);
+            if (user == null)
+                return NotFound();
+
+            var userViewModel = new UserViewModel(user.Id, user.FullName, user.Height, user.Weight, user.BirthDate);
+
+            return Ok(userViewModel);
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateUserInputModel inputModel)
+        {
+            var user = new User(inputModel.FullName, inputModel.Height, inputModel.Weight, inputModel.BirthDate);
+            _dbContext.Users.Add(user);
+            _dbContext.SaveChanges();
+            return CreatedAtAction(nameof(Get),new {id = user.Id }, inputModel);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateUserInputModel inputModel)
+        {
+            var user = _dbContext.Users.SingleOrDefault(u => u.Id == id);
+            if (user == null)
+                return NotFound();
+
+            user.Update(inputModel.Height, inputModel.Weight);
+
+            _dbContext.SaveChanges();
+            return NoContent();
+        }
+
+    }
+}
